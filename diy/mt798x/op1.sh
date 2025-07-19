2bd6e54f2f647dd0149ab8b91df44ec7d537a9da
georip <georip@163.com>
2025-07-19 08:00:01 +0800

    Update op1.sh

--- diy/mt798x/op1.sh
+++ diy/mt798x/op1.sh
@@ -4,3 +4,6 @@
 #=================================================
 ##添加自己的插件库
 echo -e "\nsrc-git extraipk https://github.com/xiangfeidexiaohuo/extra-ipk" >> feeds.conf.default
+#git clone https://github.com/sirpdboy/luci-app-eqosplus package/luci-app-eqosplus
+#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
